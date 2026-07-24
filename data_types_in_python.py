443c41770dfa2d46522c629fa8c0a014f9a719a0
ritikan <ritikan@cybage.com>
2026-07-24 11:09:38 +0530

    1st .py file added

---
+++ data_types_in_python.py
@@ -0,0 +1,18 @@
+#basic data types in python
+
+my_list = [1, 2, 3]  #list [mutable]
+my_tuple = (1, 2, 3)  #tupple (immutable)
+
+x = 10          # int
+y = 3.14        # float
+d = {"a": 1}    # dict
+s = "hello"     # str
+z = True        # bool
+
+print(type(x))
+print(type(y))
+print(type(d))
+print(type(s))
+print(type(z))
+print(type(my_list))
+print(type(my_tuple))
